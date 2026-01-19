4cc9bed45e0a564e0519bcde4d71b6ce7281fa57
catbrandt <16284@coderacademy.edu.au>
2026-01-19 21:57:37 +1100

    feat: beginning of models.booking code

--- src/models/Booking.js
+++ src/models/Booking.js
@@ -1,3 +1,11 @@
 const db = require('../config/db')
 
-const createBooking = async ()
+const createBooking = async (eventId, spaceId, UserId, quantity, totalPrice, paymentStatus) => {
+  const result = await db.query(
+    `INSERT INTO bookings (event_id, space_id, user_id, quantity, total_price, payment_status) 
+     VALUES ($1, $2, $3, $4, $5, $6) 
+     RETURNING *`,
+    [eventId, spaceId, userId, quantity, totalPrice, paymentStatus],
+  )
+  return result.rows[0]
+}
